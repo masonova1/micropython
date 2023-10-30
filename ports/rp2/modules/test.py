376fd02104748a20e556c80897d619e1cc952b87
jmtinycircuits <jmarcum@tinycircuits.com>
2023-10-30 11:07:40 -0400

    Start working on rectangle_2d_node

--- ports/rp2/modules/test.py
+++ ports/rp2/modules/test.py
@@ -1,42 +1,17 @@
 import engine
-from engine import EmptyNode, CameraNode, BitmapSpriteNode, Vector3, Rectangle
+from engine import EmptyNode, CameraNode, BitmapSpriteNode, Rectangle2DNode, Vector3, Rectangle
 import gc
 print("dir(engine):",dir(engine))
 print("dir(EmptyNode):", dir(EmptyNode))
 print("dir(CameraNode):", dir(CameraNode))
 print("dir(BitmapSpriteNode):", dir(BitmapSpriteNode))
+print("dir(Rectangle2DNode):", dir(Rectangle2DNode))
 print("dir(Vector3):", dir(Vector3))
 print("dir(Rectangle):", dir(Rectangle))
 
-# engine.debug_enable_all()
+engine.debug_enable_all()
 # engine.debug_enable_setting(engine.debug_setting_performance)
 
-class MyNodeA(EmptyNode):
-    def __init__(self):
-        super().__init__(self)
-
-    def tick(self):
-        pass
-        # print("hi from tick() A")
-
-
-class MyNodeB(EmptyNode):
-    def __init__(self):
-        super().__init__(self)
-
-
-class MyNodeC(EmptyNode):
-    def __init__(self):
-        super().__init__(self)
-
-
-class MyNodeD(EmptyNode):
-    def __init__(self):
-        super().__init__(self)
-    
-    def tick(self):
-        pass
-        # print("hi from tick() D")
 
 class MyNodeCam(CameraNode):
     def __init__(self):
@@ -46,72 +21,155 @@ class MyNodeCam(CameraNode):
         pass
         # print("hi from camera!")
 
+
 class MySprite(BitmapSpriteNode):
     def __init__(self):
         super().__init__(self)
-    
+        # self.height = 10
+
     def tick(self):
         pass
         # print("hi from sprite!")
 
 
-a = MyNodeA()
-b = MyNodeB()
-if(True):
-    c = MyNodeC()
-    del c
-d = MyNodeD()
-d.set_layer(7)
+class MyRect2D(Rectangle2DNode):
+    def __init__(self):
+        super().__init__(self)
+        # self.width = 5
+        # self.position.x = 10
+    
+    def tick(self):
+        pass
+        # print("hi from sprite!")
 
-e = MyNodeA()
-e1 = MyNodeA()
-e2 = MyNodeA()
-e3 = MyNodeA()
-e4 = MyNodeA()
-e5 = MyNodeA()
-e6 = MyNodeA()
-e7 = MyNodeA()
+a = MySprite()
+b = MyRect2D()
+c = MyNodeCam()
+print(b.width)
 
-print()
-
-
-v = Vector3()
-v.x = 2
-v.y = 2
-v.z = 2
-print(dir(v))
-print(v, end='')
+# a.position.x = 0
+# b.position.x = 10
 
 
-cam0 = MyNodeCam()
-cam1 = MyNodeCam()
-cam2 = MyNodeCam()
-cam3 = MyNodeCam()
-
-
-cam0.viewport.x=0
-cam0.viewport.y=0
-cam0.viewport.width=64
-cam0.viewport.height=64
-
-cam1.viewport.x=64
-cam1.viewport.y=0
-cam1.viewport.width=64
-cam1.viewport.height=64
-
-cam2.viewport.x=0
-cam2.viewport.y=64
-cam2.viewport.width=64
-cam2.viewport.height=64
-
-cam3.viewport.x=64
-cam3.viewport.y=64
-cam3.viewport.width=64
-cam3.viewport.height=64
-
-spr = MySprite()
-
-
-gc.collect()
+# print(dir(v))
+# print(v, end='')
 
 engine.start()
+
+
+# class MyNodeA(EmptyNode):
+#     def __init__(self):
+#         super().__init__(self)
+
+#     def tick(self):
+#         pass
+#         # print("hi from tick() A")
+
+
+# class MyNodeB(EmptyNode):
+#     def __init__(self):
+#         super().__init__(self)
+
+
+# class MyNodeC(EmptyNode):
+#     def __init__(self):
+#         super().__init__(self)
+
+
+# class MyNodeD(EmptyNode):
+#     def __init__(self):
+#         super().__init__(self)
+    
+#     def tick(self):
+#         pass
+#         # print("hi from tick() D")
+
+# class MyNodeCam(CameraNode):
+#     def __init__(self):
+#         super().__init__(self)
+    
+#     def tick(self):
+#         pass
+#         # print("hi from camera!")
+
+# class MySprite(BitmapSpriteNode):
+#     def __init__(self):
+#         super().__init__(self)
+
+#     def tick(self):
+#         pass
+#         # print("hi from sprite!")
+
+
+# class MyRect2D(Rectangle2DNode):
+#     def __init__(self):
+#         super().__init__(self)
+#         self.height = 10
+    
+#     def tick(self):
+#         pass
+#         # print("hi from sprite!")
+
+
+# a = MyNodeA()
+# b = MyNodeB()
+# if(True):
+#     c = MyNodeC()
+#     del c
+# d = MyNodeD()
+# d.set_layer(7)
+
+# e = MyNodeA()
+# e1 = MyNodeA()
+# e2 = MyNodeA()
+# e3 = MyNodeA()
+# e4 = MyNodeA()
+# e5 = MyNodeA()
+# e6 = MyNodeA()
+# e7 = MyNodeA()
+
+# print()
+
+
+# v = Vector3()
+# v.x = 2
+# v.y = 2
+# v.z = 2
+# print(dir(v))
+# print(v, end='')
+
+
+# cam0 = MyNodeCam()
+# cam1 = MyNodeCam()
+# cam2 = MyNodeCam()
+# cam3 = MyNodeCam()
+
+
+# cam0.viewport.x=0
+# cam0.viewport.y=0
+# cam0.viewport.width=64
+# cam0.viewport.height=64
+
+# cam1.viewport.x=64
+# cam1.viewport.y=0
+# cam1.viewport.width=64
+# cam1.viewport.height=64
+
+# cam2.viewport.x=0
+# cam2.viewport.y=64
+# cam2.viewport.width=64
+# cam2.viewport.height=64
+
+# cam3.viewport.x=64
+# cam3.viewport.y=64
+# cam3.viewport.width=64
+# cam3.viewport.height=64
+
+# spr = MySprite()
+
+# r = MyRect2D()
+
+
+# gc.collect()
+
+# engine.start()
