69363f7aacedbab535f1c6cb315e668536cda7b4
Mason W <masonova@gmail.com>
2023-11-02 14:46:23 -0400

    Fix rectangle / blit clipping and drawing

--- examples/usercmodule/engine/display/engine_display_draw.c
+++ examples/usercmodule/engine/display/engine_display_draw.c
@@ -60,11 +60,17 @@ void engine_draw_pixel(uint16_t color, int32_t x, int32_t y, engine_camera_node_
 void engine_fill_color(uint16_t color, int32_t x, int32_t y, int32_t width, int32_t height, engine_camera_node_class_obj_t *camera) {
     uint16_t *screen_buffer = engine_get_active_screen_buffer();
     if(camera == NULL){
-        if(x < 0) x = 0;
+        if(x < 0) {
+            width += x;
+            x = 0;
+        }
+        if(y < 0)  {
+            height += y;
+            y = 0;
+        }
         if(x + width > SCREEN_WIDTH) width = SCREEN_WIDTH - x;
-        if(y < 0) y = 0;
         if(y + height > SCREEN_HEIGHT) height = SCREEN_HEIGHT - y;
-        if(width != 0 && height != 0){
+        if(width > 0 && height > 0){
             //screen_buffer[y*SCREEN_WIDTH + x] = color;
             int32_t pos = y*SCREEN_WIDTH + x;
             const int32_t width_difference = SCREEN_WIDTH - width;
@@ -78,19 +84,31 @@ void engine_fill_color(uint16_t color, int32_t x, int32_t y, int32_t width, int3
             ENGINE_WARNING_PRINTF("Tried to draw zero-size rectangle");
         }
     }else{
+
         vector3_class_obj_t *camera_position = camera->position;
         rectangle_class_obj_t *camera_viewport = camera->viewport;
         vector2_class_obj_t* pos = MP_OBJ_TO_PTR(camera_viewport->pos);
         vector2_class_obj_t* size = MP_OBJ_TO_PTR(camera_viewport->size);
 
+        ENGINE_WARNING_PRINTF("Viewport rectangle at (%0.3f, %0.3f) with size (%0.3f, %0.3f)", (double)mp_obj_get_float(pos->x), (double)mp_obj_get_float(pos->y), (double)mp_obj_get_float(size->x), (double)mp_obj_get_float(size->y));
+
         int32_t result_x = ((int32_t)mp_obj_get_float(pos->x)) + (x - (int32_t)mp_obj_get_float(camera_position->x));
         int32_t result_y = ((int32_t)mp_obj_get_float(pos->y)) + (y - (int32_t)mp_obj_get_float(camera_position->y));
-        if(result_x < (int32_t)mp_obj_get_float(pos->x)) x = mp_obj_get_float(pos->x);
+        if(result_x < (int32_t)mp_obj_get_float(pos->x)) {
+            width -= (int32_t)mp_obj_get_float(pos->x) - result_x;
+            result_x = (int32_t)mp_obj_get_float(pos->x);
+        }
+        if(result_y < (int32_t)mp_obj_get_float(pos->y)) {
+            height -= (int32_t)mp_obj_get_float(pos->y) - result_y;
+            result_y = (int32_t)mp_obj_get_float(pos->y);
+        }
         if(result_x + width > (int32_t)mp_obj_get_float(size->x)) width = (int32_t)mp_obj_get_float(size->x) - result_x;
-        if(result_y < (int32_t)mp_obj_get_float(pos->y)) y = mp_obj_get_float(pos->y);
         if(result_y + height > (int32_t)mp_obj_get_float(size->y)) height = (int32_t)mp_obj_get_float(size->y) - result_y;
 
-        if(width != 0 && height != 0){
+        ENGINE_WARNING_PRINTF("Drawing rectangle at (%0.3f, %0.3f) with size (%0.3f, %0.3f)", (double)result_x, (double)result_y, (double)width, (double)height);
+
+
+        if(width > 0 && height > 0){
             int32_t pos = result_y*SCREEN_WIDTH + result_x;
             const int32_t width_difference = SCREEN_WIDTH - width;
             for(int y = 0; y < height; y++) {
@@ -113,10 +131,20 @@ void engine_draw_fill_screen_buffer(uint16_t color, uint16_t *screen_buffer){
 void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, int32_t width, int32_t height, engine_camera_node_class_obj_t *camera){
     uint16_t *screen_buffer = engine_get_active_screen_buffer();
     const int32_t old_width = width;
+    int32_t p_add = 0;
+    int32_t p_offs = 0;
     if(camera == NULL){
-        if(x < 0) x = 0;
+        if(y < 0) {
+            p_offs = -y * width;
+            height += y;
+            y = 0;
+        }
+        if(x < 0) {
+            p_add = -x;
+            width += x;
+            x = 0;
+        }
         if(x + width > SCREEN_WIDTH) width = SCREEN_WIDTH - x;
-        if(y < 0) y = 0;
         if(y + height > SCREEN_HEIGHT) height = SCREEN_HEIGHT - y;
         if(width != 0 && height != 0){
             //screen_buffer[y*SCREEN_WIDTH + x] = color;
@@ -126,10 +154,10 @@ void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, int32_t width, int
             const int32_t p_width_difference = old_width - width;
             for(int y = 0; y < height; y++) {
                 for(int x = 0; x < width; x++) {
-                    screen_buffer[pos++] = pixels[p_pos++];
+                    screen_buffer[pos++] = pixels[p_offs + p_pos++];
                 }
                 pos += width_difference;
-                p_pos += p_width_difference;
+                p_pos += p_width_difference + p_add;
             }
         }else{
             ENGINE_WARNING_PRINTF("Tried to draw zero-size rectangle");
@@ -142,9 +170,17 @@ void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, int32_t width, int
 
         int32_t result_x = ((int32_t)mp_obj_get_float(pos->x)) + (x - (int32_t)mp_obj_get_float(camera_position->x));
         int32_t result_y = ((int32_t)mp_obj_get_float(pos->y)) + (y - (int32_t)mp_obj_get_float(camera_position->y));
-        if(result_x < (int32_t)mp_obj_get_float(pos->x)) x = mp_obj_get_float(pos->x);
+        if(result_y < (int32_t)mp_obj_get_float(pos->y)) {
+            p_offs = ((int32_t)mp_obj_get_float(pos->y) - result_y) * width;
+            height -= (int32_t)mp_obj_get_float(pos->x) - result_x;
+            y = mp_obj_get_float(pos->y);
+        }
+        if(result_x < (int32_t)mp_obj_get_float(pos->x)) {
+            p_add = (int32_t)mp_obj_get_float(pos->x) - result_x;
+            width -= (int32_t)mp_obj_get_float(pos->x) - result_x;
+            x = mp_obj_get_float(pos->x);
+        }
         if(result_x + width > (int32_t)mp_obj_get_float(size->x)) width = (int32_t)mp_obj_get_float(size->x) - result_x;
-        if(result_y < (int32_t)mp_obj_get_float(pos->y)) y = mp_obj_get_float(pos->y);
         if(result_y + height > (int32_t)mp_obj_get_float(size->y)) height = (int32_t)mp_obj_get_float(size->y) - result_y;
 
         if(width != 0 && height != 0){
@@ -154,10 +190,10 @@ void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, int32_t width, int
             const int32_t p_width_difference = old_width - width;
             for(int y = 0; y < height; y++) {
                 for(int x = 0; x < width; x++) {
-                    screen_buffer[pos++] = pixels[p_pos++];
+                    screen_buffer[pos++] = pixels[p_offs + p_pos++];
                 }
                 pos += width_difference;
-                p_pos += p_width_difference;
+                p_pos += p_width_difference + p_add;
             }
         }else{
             ENGINE_WARNING_PRINTF("Tried to draw zero-size rectangle");
--- examples/usercmodule/engine/nodes/2D/rectangle_2d_node.c
+++ examples/usercmodule/engine/nodes/2D/rectangle_2d_node.c
@@ -14,50 +14,6 @@ STATIC void rectangle_2d_node_class_print(const mp_print_t *print, mp_obj_t self
     ENGINE_INFO_PRINTF("print(): Rectangle2DNode");
 }
 
-mp_obj_t rectangle_2d_node_class_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args){
-    ENGINE_INFO_PRINTF("New Rectangle2DNode");
-
-    // Check that there's an argument that's hopefully a reference to the inheriting subclass
-    mp_arg_check_num(n_args, n_kw, 1, 1, true);
-
-    // How to make __del__ get called when object is garbage collected: https://github.com/micropython/micropython/issues/1878
-    // Why it might get called early: https://forum.micropython.org/viewtopic.php?t=1405 (make sure the object is actually returned from this function)
-    engine_rectangle_2d_node_class_obj_t *self = m_new_obj_with_finaliser(engine_rectangle_2d_node_class_obj_t);
-    self->base.type = &engine_rectangle_2d_node_class_type;
-    self->node_base.layer = 0;
-    self->node_base.type = NODE_TYPE_RECTANGLE_2D;
-    self->node_base.object_list_node = engine_add_object_to_layer(self, self->node_base.layer);
-    node_base_set_if_visible(&self->node_base, true);
-    node_base_set_if_disabled(&self->node_base, false);
-    node_base_set_if_just_added(&self->node_base, true);
-
-    // Cache lookup results of 'tick()' and 'draw()' functions on this
-    // node instance so that the main engine loop can call it quickly
-    mp_load_method(MP_OBJ_TO_PTR(args[0]), MP_QSTR_tick, self->tick_dest);
-    mp_load_method(MP_OBJ_TO_PTR(args[0]), MP_QSTR_draw, self->draw_dest);
-
-    ENGINE_INFO_PRINTF("Creating new Vector2 for BitmapSprite Node");
-    // self->position = vector2_class_new(&vector2_class_type, 0, 0, NULL);
-    // self->width = mp_obj_new_int(15);
-    // self->height = mp_obj_new_int(5);
-    self->rect = rectangle_class_new(&rectangle_class_type, 0, 0, NULL);
-    self->color = mp_obj_new_int(0xffff);
-
-    return MP_OBJ_FROM_PTR(self);
-}
-
-
-// Class methods
-STATIC mp_obj_t rectangle_2d_node_class_del(mp_obj_t self_in){
-    ENGINE_INFO_PRINTF("Rectangle2DNode: Deleted (garbage collected, removing self from active engine objects)");
-
-    engine_rectangle_2d_node_class_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    engine_remove_object_from_layer(self->node_base.object_list_node, self->node_base.layer);
-
-    return mp_const_none;
-}
-MP_DEFINE_CONST_FUN_OBJ_1(rectangle_2d_node_class_del_obj, rectangle_2d_node_class_del);
-
 
 STATIC mp_obj_t rectangle_2d_node_class_tick(mp_obj_t self_in){
     ENGINE_WARNING_PRINTF("Rectangle2DNode: Tick function not overridden");
@@ -70,25 +26,84 @@ STATIC mp_obj_t rectangle_2d_node_class_draw(mp_obj_t self_in, mp_obj_t camera_o
     ENGINE_INFO_PRINTF("Rectangle2DNode: Drawing");
 
     engine_rectangle_2d_node_class_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    //ENGINE_FORCE_PRINTF("TEST %lu", self->rect->size.x);
-    // vector2_class_obj_t *self_position = self->position;
+    engine_camera_node_class_obj_t *camera = MP_OBJ_TO_PTR(camera_obj);
 
-    // engine_camera_node_class_obj_t *camera = MP_OBJ_TO_PTR(camera_obj);
+    // For whatever reason, position being a struct within self
+    // means that we don't need look up the position attribute.
+    // Only things like self.width need to be looked up
+    vector2_class_obj_t *position = self->position;
+    mp_int_t width = mp_obj_get_int(mp_load_attr(self->access, MP_QSTR_width));
+    mp_int_t height = mp_obj_get_int(mp_load_attr(self->access, MP_QSTR_height));
+    mp_int_t color = mp_obj_get_int(mp_load_attr(self->access, MP_QSTR_color));
 
-    // ENGINE_FORCE_PRINTF("TEST %lu", self->height);
-
-    // // Rotation not implemented yet so this is simple!
-    // for(mp_uint_t y=0; y<5; y++){
-    //     for(mp_uint_t x=0; x<self->width; x++){
-    //         ENGINE_FORCE_PRINTF("%lu", self_position->x+x);
-    //         ENGINE_FORCE_PRINTF("%lu", self_position->y+y);
-    //         engine_draw_pixel(self->color, (int32_t)self_position->x+x, (int32_t)self_position->y+y, camera);
+    // Rotation not implemented yet so this is simple!
+    // for(mp_int_t y=0; y<height; y++){
+    //     for(mp_int_t x=0; x<width; x++){
+    //         engine_draw_pixel(color, (int32_t)mp_obj_get_float(position->x)+x, (int32_t)mp_obj_get_float(position->y)+y, camera);
     //     }
     // }
+    engine_fill_color(color, (int32_t)mp_obj_get_float(position->x), (int32_t)mp_obj_get_float(position->y), width, height, camera);
 
     return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_2(rectangle_2d_node_class_draw_obj, rectangle_2d_node_class_draw);
+
+
+mp_obj_t rectangle_2d_node_class_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args){
+    ENGINE_INFO_PRINTF("New Rectangle2DNode");
+
+    // Check that there's an argument that's hopefully a reference to the inheriting subclass
+    mp_arg_check_num(n_args, n_kw, 0, 1, true);
+
+    // How to make __del__ get called when object is garbage collected: https://github.com/micropython/micropython/issues/1878
+    // Why it might get called early: https://forum.micropython.org/viewtopic.php?t=1405 (make sure the object is actually returned from this function)
+    engine_rectangle_2d_node_class_obj_t *self = m_new_obj_with_finaliser(engine_rectangle_2d_node_class_obj_t);
+    self->base.type = &engine_rectangle_2d_node_class_type;
+    self->node_base.layer = 0;
+    self->node_base.type = NODE_TYPE_RECTANGLE_2D;
+    self->node_base.object_list_node = engine_add_object_to_layer(self, self->node_base.layer);
+    node_base_set_if_visible(&self->node_base, true);
+    node_base_set_if_disabled(&self->node_base, false);
+    node_base_set_if_just_added(&self->node_base, true);
+
+    // Handle setting callbacks depending on if this class was inherited or not
+    if(n_args == 0){
+        ENGINE_INFO_PRINTF("Rectangle2DNode: Does not have child class");
+        self->access = self;
+        self->tick_dest[0] = MP_OBJ_FROM_PTR(&rectangle_2d_node_class_tick_obj);
+        self->tick_dest[1] = self;
+
+        self->draw_dest[0] = MP_OBJ_FROM_PTR(&rectangle_2d_node_class_draw_obj);
+    }else{
+        ENGINE_INFO_PRINTF("Rectangle2DNode: Does have child class");
+        self->access = args[0];
+        mp_load_method(self->access, MP_QSTR_tick, self->tick_dest);
+        mp_load_method(self->access, MP_QSTR_draw, self->draw_dest);
+    }
+
+
+    ENGINE_INFO_PRINTF("Creating new Vector2 for BitmapSprite Node");
+    self->position = vector2_class_new(&vector2_class_type, 0, 0, NULL);
+    self->width = mp_obj_new_int(15);
+    self->height = mp_obj_new_int(5);
+    self->color = mp_obj_new_int(0xffff);
+
+    self->draw_dest[1] = self;
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
+
+// Class methods
+STATIC mp_obj_t rectangle_2d_node_class_del(mp_obj_t self_in){
+    ENGINE_WARNING_PRINTF("Rectangle2DNode: Deleted (garbage collected, removing self from active engine objects)");
+
+    engine_rectangle_2d_node_class_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    engine_remove_object_from_layer(self->node_base.object_list_node, self->node_base.layer);
+
+    return mp_const_none;
+}
+MP_DEFINE_CONST_FUN_OBJ_1(rectangle_2d_node_class_del_obj, rectangle_2d_node_class_del);
 
 
 STATIC mp_obj_t rectangle_2d_node_class_set_layer(mp_obj_t self_in, mp_obj_t layer){
@@ -111,20 +126,43 @@ MP_DEFINE_CONST_FUN_OBJ_2(rectangle_2d_node_class_set_layer_obj, rectangle_2d_no
 STATIC void rectangle_2d_class_attr(mp_obj_t self_in, qstr attribute, mp_obj_t *destination){
     ENGINE_INFO_PRINTF("Accessing Rectangle2DNode attr");
 
-    engine_rectangle_2d_node_class_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    engine_rectangle_2d_node_class_obj_t *self = self_in;
 
     if(destination[0] == MP_OBJ_NULL){          // Load
         switch(attribute){
-            // case MP_QSTR_rect: destination[0] = rectangle_class_copy((rectangle_class_obj_t*)MP_OBJ_TO_PTR(self->rect)); break;
-            case MP_QSTR_rect: destination[0] = self->rect; break;
-            default: break;
+            case MP_QSTR___del__:
+                destination[0] = MP_OBJ_FROM_PTR(&rectangle_2d_node_class_del_obj);
+                destination[1] = self_in;
+            break;
+            case MP_QSTR_position:
+                destination[0] = self->position;
+            break;
+            case MP_QSTR_width:
+                destination[0] = self->width;
+            break;
+            case MP_QSTR_height:
+                destination[0] = self->height;
+            break;
+            case MP_QSTR_color:
+                destination[0] = self->color;
+            break;
+            default:
+                return; // Fail
         }
     }else if(destination[1] != MP_OBJ_NULL){    // Store
         switch(attribute){
-            case MP_QSTR_rect:
-                ENGINE_WARNING_PRINTF("Setting rectangle");
-                //self->rect = rectangle_class_copy((rectangle_class_obj_t*)MP_OBJ_TO_PTR(destination[1]));
-                self->rect = destination[1]; break;
+            case MP_QSTR_position:
+                self->position = destination[1];
+            break;
+            case MP_QSTR_width:
+                self->width = destination[1];
+            break;
+            case MP_QSTR_height:
+                self->height = destination[1];
+            break;
+            case MP_QSTR_color:
+                self->color = destination[1];
+            break;
             default:
                 return; // Fail
         }
@@ -140,7 +178,7 @@ STATIC const mp_rom_map_elem_t rectangle_2d_node_class_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR___del__),     MP_ROM_PTR(&rectangle_2d_node_class_del_obj) },
     { MP_ROM_QSTR(MP_QSTR_tick),        MP_ROM_PTR(&rectangle_2d_node_class_tick_obj) },
     { MP_ROM_QSTR(MP_QSTR_draw),        MP_ROM_PTR(&rectangle_2d_node_class_draw_obj) },
-    { MP_ROM_QSTR(MP_QSTR_set_layer),   MP_ROM_PTR(&rectangle_2d_node_class_set_layer_obj) },
+    { MP_ROM_QSTR(MP_QSTR_set_layer),   MP_ROM_PTR(&rectangle_2d_node_class_set_layer_obj) }
 };
 
 
