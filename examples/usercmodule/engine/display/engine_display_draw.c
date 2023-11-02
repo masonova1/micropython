af5d4eb17b2ece680ff2b3fd8219fdca6beb4d9d
Mason W <masonova@gmail.com>
2023-11-02 13:25:35 -0400

    Add blit, expose fill/blit functions through header

--- examples/usercmodule/engine/display/engine_display_draw.c
+++ examples/usercmodule/engine/display/engine_display_draw.c
@@ -57,19 +57,19 @@ void engine_draw_pixel(uint16_t color, int32_t x, int32_t y, engine_camera_node_
     }
 }
 
-void engine_fill_color(uint16_t color, int32_t x, int32_t y, int32_t sx, int32_t sy, engine_camera_node_class_obj_t *camera) {
+void engine_fill_color(uint16_t color, int32_t x, int32_t y, int32_t width, int32_t height, engine_camera_node_class_obj_t *camera) {
     uint16_t *screen_buffer = engine_get_active_screen_buffer();
     if(camera == NULL){
         if(x < 0) x = 0;
-        if(x + sx > SCREEN_WIDTH) sx = SCREEN_WIDTH - x;
+        if(x + width > SCREEN_WIDTH) width = SCREEN_WIDTH - x;
         if(y < 0) y = 0;
-        if(y + sy > SCREEN_HEIGHT) sy = SCREEN_HEIGHT - y;
-        if(sx != 0 && sy != 0){
+        if(y + height > SCREEN_HEIGHT) height = SCREEN_HEIGHT - y;
+        if(width != 0 && height != 0){
             //screen_buffer[y*SCREEN_WIDTH + x] = color;
             int32_t pos = y*SCREEN_WIDTH + x;
-            const int32_t width_difference = SCREEN_WIDTH - sx;
-            for(int y = 0; y < sy; y++) {
-                for(int x = 0; x < sx; x++) {
+            const int32_t width_difference = SCREEN_WIDTH - width;
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
                     screen_buffer[pos++] = color;
                 }
                 pos += width_difference;
@@ -86,15 +86,15 @@ void engine_fill_color(uint16_t color, int32_t x, int32_t y, int32_t sx, int32_t
         int32_t result_x = ((int32_t)mp_obj_get_float(pos->x)) + (x - (int32_t)mp_obj_get_float(camera_position->x));
         int32_t result_y = ((int32_t)mp_obj_get_float(pos->y)) + (y - (int32_t)mp_obj_get_float(camera_position->y));
         if(result_x < (int32_t)mp_obj_get_float(pos->x)) x = mp_obj_get_float(pos->x);
-        if(result_x + sx > (int32_t)mp_obj_get_float(size->x)) sx = (int32_t)mp_obj_get_float(size->x) - result_x;
+        if(result_x + width > (int32_t)mp_obj_get_float(size->x)) width = (int32_t)mp_obj_get_float(size->x) - result_x;
         if(result_y < (int32_t)mp_obj_get_float(pos->y)) y = mp_obj_get_float(pos->y);
-        if(result_y + sy > (int32_t)mp_obj_get_float(size->y)) sy = (int32_t)mp_obj_get_float(size->y) - result_y;
+        if(result_y + height > (int32_t)mp_obj_get_float(size->y)) height = (int32_t)mp_obj_get_float(size->y) - result_y;
 
-        if(sx != 0 && sy != 0){
+        if(width != 0 && height != 0){
             int32_t pos = result_y*SCREEN_WIDTH + result_x;
-            const int32_t width_difference = SCREEN_WIDTH - sx;
-            for(int y = 0; y < sy; y++) {
-                for(int x = 0; x < sx; x++) {
+            const int32_t width_difference = SCREEN_WIDTH - width;
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
                     screen_buffer[pos++] = color;
                 }
                 pos += width_difference;
@@ -110,16 +110,52 @@ void engine_draw_fill_screen_buffer(uint16_t color, uint16_t *screen_buffer){
 }
 
 
-void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, uint16_t width, uint16_t height){
-    // if(x>=SCREEN_WIDTH || y>=SCREEN_HEIGHT){
-    //     ENGINE_WARNING_PRINTF("Tried to blit of screen bounds");
-    //     return;
-    // }
+void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, int32_t width, int32_t height, engine_camera_node_class_obj_t *camera){
+    uint16_t *screen_buffer = engine_get_active_screen_buffer();
+    if(camera == NULL){
+        if(x < 0) x = 0;
+        if(x + width > SCREEN_WIDTH) width = SCREEN_WIDTH - x;
+        if(y < 0) y = 0;
+        if(y + height > SCREEN_HEIGHT) height = SCREEN_HEIGHT - y;
+        if(width != 0 && height != 0){
+            //screen_buffer[y*SCREEN_WIDTH + x] = color;
+            int32_t pos = y*SCREEN_WIDTH + x;
+            int32_t p_pos = 0;
+            const int32_t width_difference = SCREEN_WIDTH - width;
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
+                    screen_buffer[pos++] = pixels[p_pos++];
+                }
+                pos += width_difference;
+            }
+        }else{
+            ENGINE_WARNING_PRINTF("Tried to draw zero-size rectangle");
+        }
+    }else{
+        vector3_class_obj_t *camera_position = camera->position;
+        rectangle_class_obj_t *camera_viewport = camera->viewport;
+        vector2_class_obj_t* pos = MP_OBJ_TO_PTR(camera_viewport->pos);
+        vector2_class_obj_t* size = MP_OBJ_TO_PTR(camera_viewport->size);
 
-    // uint16_t *screen_buffer = engine_get_active_screen_buffer();
-    // uint16_t pixels_in_screen_buffer_start = (y * width) + x;
+        int32_t result_x = ((int32_t)mp_obj_get_float(pos->x)) + (x - (int32_t)mp_obj_get_float(camera_position->x));
+        int32_t result_y = ((int32_t)mp_obj_get_float(pos->y)) + (y - (int32_t)mp_obj_get_float(camera_position->y));
+        if(result_x < (int32_t)mp_obj_get_float(pos->x)) x = mp_obj_get_float(pos->x);
+        if(result_x + width > (int32_t)mp_obj_get_float(size->x)) width = (int32_t)mp_obj_get_float(size->x) - result_x;
+        if(result_y < (int32_t)mp_obj_get_float(pos->y)) y = mp_obj_get_float(pos->y);
+        if(result_y + height > (int32_t)mp_obj_get_float(size->y)) height = (int32_t)mp_obj_get_float(size->y) - result_y;
 
-    // for(uint16_t ihx=0; ihx<height; ihx++){
-    //     for(uint16_t width, )
-    // }
+        if(width != 0 && height != 0){
+            int32_t pos = result_y*SCREEN_WIDTH + result_x;
+            int32_t p_pos = 0;
+            const int32_t width_difference = SCREEN_WIDTH - width;
+            for(int y = 0; y < height; y++) {
+                for(int x = 0; x < width; x++) {
+                    screen_buffer[pos++] = pixels[p_pos++];
+                }
+                pos += width_difference;
+            }
+        }else{
+            ENGINE_WARNING_PRINTF("Tried to draw zero-size rectangle");
+        }
+    }
 }
--- examples/usercmodule/engine/display/engine_display_draw.h
+++ examples/usercmodule/engine/display/engine_display_draw.h
@@ -11,7 +11,10 @@ void engine_draw_pixel(uint16_t color, int32_t x, int32_t y, engine_camera_node_
 // Fill entire screen buffer with a color
 void engine_draw_fill_screen_buffer(uint16_t color, uint16_t *screen_buffer);
 
+// Fill rectangle at 'x' and 'y' given 'width' and 'height' in color 'color'
+void engine_fill_color(uint16_t color, int32_t x, int32_t y, int32_t width, int32_t height, engine_camera_node_class_obj_t *camera);
+
 // Blits pixels to screen at 'x' and 'y' given 'width' and 'height'
-void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, uint16_t width, uint16_t height);
+void engine_draw_blit(uint16_t *pixels, int32_t x, int32_t y, int32_t width, int32_t height, engine_camera_node_class_obj_t *camera);
 
 #endif  // ENGINE_DISPLAY_DRAW_H
