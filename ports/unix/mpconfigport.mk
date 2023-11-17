3d6dd5597163f5b9f13410882bd997e4ce26cbad
jmtinycircuits <jmarcum@tinycircuits.com>
2023-11-17 16:31:30 -0500

    Get c++ and box2d compiling on UNIX port

--- ports/unix/mpconfigport.mk
+++ ports/unix/mpconfigport.mk
@@ -9,25 +9,25 @@ MICROPY_FORCE_32BIT = 0
 MICROPY_USE_READLINE = 1
 
 # btree module using Berkeley DB 1.xx
-MICROPY_PY_BTREE = 1
+MICROPY_PY_BTREE = 0
 
 # _thread module using pthreads
-MICROPY_PY_THREAD = 1
+MICROPY_PY_THREAD = 0
 
 # Subset of CPython termios module
 MICROPY_PY_TERMIOS = 1
 
 # Subset of CPython socket module
-MICROPY_PY_SOCKET = 1
+MICROPY_PY_SOCKET = 0
 
 # ffi module requires libffi (libffi-dev Debian package)
 MICROPY_PY_FFI = 1
 
 # ussl module requires one of the TLS libraries below
-MICROPY_PY_USSL = 1
+MICROPY_PY_USSL = 0
 # axTLS has minimal size but implements only a subset of modern TLS
 # functionality, so may have problems with some servers.
-MICROPY_SSL_AXTLS = 1
+MICROPY_SSL_AXTLS = 0
 # mbedTLS is more up to date and complete implementation, but also
 # more bloated.
 MICROPY_SSL_MBEDTLS = 0
